4a95154a570f14feff74f5ecea1402476f968ed7
ajzajac <ajzajac@icloud.com>
2020-07-05 23:43:44 -0400

    first commit includes temp front page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import flowers from './images/flowers.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='landing-page-left'>
+          <p>anx-ish</p>
+          <p>...coming soon!</p>
+        </div>
+        <div className='landing-page-right'>
+          <img src={flowers}></img>
+        </div>
     </div>
   );
 }
