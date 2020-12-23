a81a13cec039929bdf23babe102729e1e83656d1
ajzajac <ajzajac@icloud.com>
2020-12-23 14:55:28 -0500

    new containers and components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
-
+import MainContainer from './containers/MainContainer'
 
 function App() {
   return (
     <div className="App">
         <div className='landing-page-temp'>
-          <p>anx-ish</p>
+          {/* <p>anx-ish</p>
           <p>has your mental health nose-dived in quarantine?</p>
           <p style={{marginLeft: "15%", marginTop: "3%", fontFamily: 'Montserrat Subrayada, sans-serif', textShadow: "1px 1px 8px rgb(243, 187, 138)" }}>join the club.</p>
-          <p>COMING SOON!</p>
+          <p>COMING SOON!</p> */}
+          <MainContainer />
         </div>
     </div>
   );
